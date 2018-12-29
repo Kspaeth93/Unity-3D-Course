6e54bbdd55b3753cb1b525337bc2c7c8dbea1ce1
Kevin Spaeth <kevin.spaeth93@gmail.com>
2018-12-28 18:06:29 -0600

    Added spaceship brbrbrbrbrrrvrrr

--- 3 - Planet Express/Assets/Spaceship.cs
+++ 3 - Planet Express/Assets/Spaceship.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Spaceship : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,12 +24,22 @@ public class Spaceship : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddForce(Vector3.up);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.RightArrow))
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(-Vector3.forward);
         }
